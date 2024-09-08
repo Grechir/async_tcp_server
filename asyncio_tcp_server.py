8bc9c500d8ffffa3a77591b4f5f184a7f1859470
Grechir <kochegarov.kolya01@mail.ru>
2024-09-08 23:18:54 +0300

    first commit

---
+++ asyncio_tcp_server.py
@@ -0,0 +1,97 @@
+import asyncio
+import random
+from datetime import datetime
+
+SERVER_ADDRESS = ('127.0.0.1', 9090)
+
+
+class Server:
+    def __init__(self):
+        self.client_counter = 0
+        self.response_counter = 0
+        self.response_counter_lock = asyncio.Lock()
+
+    @staticmethod
+    async def logger(request_time, request_message, response_time=None, response_message=None):
+        with open('logs/server_log.txt', 'a', encoding='utf-8') as log_file:
+            if response_message is None:
+                log_file.write(f'{request_time}; {request_message}; (проигнорировано)\n')
+            else:
+                log_file.write(f'{request_time};{request_message};{response_time};{response_message}')
+
+    async def client_connection(self, reader, writer):
+        self.client_counter += 1
+        client_id = self.client_counter
+        request_counter = 0
+
+        async def send_keepalive():
+            while True:
+                await asyncio.sleep(5)
+                # async with self.response_counter_lock:
+                keepalive_message = f'[{self.response_counter}] keepalive\n'
+                self.response_counter += 1
+                writer.write(keepalive_message.encode('ascii'))
+                await writer.drain()
+
+        send_keepalive_message = asyncio.create_task(send_keepalive())
+
+        try:
+            # запрос клиента
+            while True:
+                data = await reader.read(1024)  # получение даты пакетами
+                if not data:
+                    break
+
+                request_time = datetime.now().strftime('%Y-%m-%d;%H:%M:%S.%f')[:-3]
+                request_message = data.decode('ascii').strip('\n')
+
+            # ответ клиенту
+                if random.random() < 0.1:
+                    await self.logger(request_time, request_message,)  # сразу logger
+                    request_counter += 1  # запрос проигнорирован, но факт запроса фиксируется в логах
+                    continue
+
+                await asyncio.sleep(random.uniform(0.1, 1))
+
+                response_time = datetime.now().strftime('%H:%M:%S.%f')[:-3]
+                async with self.response_counter_lock:  # Блокировка перед обновлением response_counter
+                    response_message = f'[{self.response_counter}/{request_counter}] PONG ({client_id})\n'
+                    self.response_counter += 1
+
+                request_counter += 1
+
+                writer.write(response_message.encode('ascii'))
+                await writer.drain()  # проверка доставки сообщения
+                await self.logger(
+                    request_time,
+                    request_message,
+                    response_time,
+                    response_message
+                )  # записываем логи
+        except asyncio.CancelledError:
+            pass
+        finally:
+            send_keepalive_message.cancel()
+            writer.close()
+            await writer.wait_closed()
+
+
+async def main():
+    server = Server()
+    open_server = await asyncio.start_server(server.client_connection, *SERVER_ADDRESS)  # создание сервера
+    async with open_server:
+
+        print('сервер запущен на 5 минут')
+        await asyncio.sleep(300)  # Ждем 5 минут (300 секунд)
+        open_server.close()
+        await open_server.wait_closed()
+        print('Отключаем сервер')
+
+        # await open_server.serve_forever()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
---
+++ clients.py
@@ -0,0 +1,94 @@
+import asyncio
+import random
+from datetime import datetime
+
+SERVER_ADDRESS = ('127.0.0.1', 9090)
+TIMEOUT_SEC = 2
+
+
+async def logger(log_file, request_time, request_message, response_time, response_message):
+    with open(log_file, 'a', encoding='utf-8') as lf:
+        if 'keepalive' in response_message:
+            lf.write(f'{response_time}; {response_message}')
+        else:
+            try:
+                lf.write(f'{request_time}; {request_message};'
+                         f' {response_time}; {response_message}'.replace('\n', '', 1))
+            except asyncio.TimeoutError:
+                lf.write(f'{request_time};{request_message};'
+                         f'{response_time};(таймаут)')
+
+
+async def client(client_id):
+    # создание клиента и его лог файла
+    reader, writer = await asyncio.open_connection(*SERVER_ADDRESS)
+    log_file = f"logs/client_{client_id}_log.txt"
+    request_num = 0
+
+    try:
+        while True:
+            await asyncio.sleep(random.uniform(0.3, 3))
+
+            # отправляет запрос
+            request_message = f'[{request_num}] PING\n'
+            request_time = datetime.now().strftime('%Y-%m-%d;%H:%M:%S.%f')[:-3]
+            writer.write(request_message.encode('ascii'))
+            await writer.drain()
+
+            # получает ответ
+            try:
+                data = await asyncio.wait_for(reader.readuntil(b'\n'), timeout=TIMEOUT_SEC)
+
+                if data:
+                    response_message = data.decode('ascii')
+                    response_time = datetime.now().strftime('%H:%M:%S.%f')[:-3]
+                    await logger(
+                        log_file,
+                        request_time,
+                        request_message,
+                        response_time,
+                        response_message
+                    )
+                else:
+                    break
+
+            except asyncio.TimeoutError:
+                await logger(
+                    log_file,
+                    request_time,
+                    request_message,
+                    TIMEOUT_SEC,
+                    '(таймаут)\n'
+                )
+
+            request_num += 1
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+async def main():
+    # Запускаем двух клиентов параллельно
+    await asyncio.gather(
+        client(1),
+        client(2),
+    )
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
